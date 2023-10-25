9abc6c30acdcaafcc7dd8051ce1609d9caf5a14d
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-10-26 01:23:48 +0200

    first version finished

--- java/first-page-java.js
+++ java/first-page-java.js
@@ -11,3 +11,24 @@ showAnimalChoice.onclick = function(){
       animalChoiceButtons.style.display = 'flex';
   }
 }
+
+let sheepAudio = document.getElementById('sheeps')
+let audioSheep = document.getElementById('audioSheep');
+
+sheepAudio.onclick = function(){
+  audioSheep.play();
+}
+
+let catAudio = document.getElementById('cats')
+let audioCat = document.getElementById('audioCat');
+
+catAudio.onclick = function(){
+  audioCat.play();
+}
+
+let alpacaAudio = document.getElementById('alpacas')
+let audioAlpaca = document.getElementById('audioAlpaca');
+
+alpacaAudio.onclick = function(){
+  audioAlpaca.play();
+}
