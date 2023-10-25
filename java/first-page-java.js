3a343caabd227ca3d6245c0cf93ea36c4b40895c
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-10-25 17:31:04 +0200

    java for buttons on/off added

---
+++ java/first-page-java.js
@@ -0,0 +1,13 @@
+let showAnimalChoice = document.getElementById('fall-down');
+let animalChoiceButtons = document.getElementById('animal-choice');
+let clickCounter = 0;
+
+showAnimalChoice.onclick = function(){
+  clickCounter++;
+    if (clickCounter % 2 == 0) {
+      animalChoiceButtons.style.display = 'none';
+  } else {
+      animalChoiceButtons.style.display = 'flex';
+  }
+}
+
